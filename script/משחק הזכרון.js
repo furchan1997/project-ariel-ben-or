4f167edfe6c2e39a8d05515f44bbf04bc1aa10c4
ariel <arielhodaya@gmail.com>
2024-06-05 02:05:54 +0300

    new

--- script/משחק הזכרון.js
+++ script/משחק הזכרון.js
@@ -1,6 +1,6 @@
 const board = document.querySelector("#board");
 const arryImges = ['אננס.jpg', 'בצל.jpg', 'חסה.jpg', 'מלפפון.jpg', 'מנגו.jpg', 'עגבניה.jpg', 'פלפל אדום מתוק.jpg', 'פלפל ירוק חריף.jpg', 'צנון.jpg', 'שום.jpg', 'אננס.jpg', 'בצל.jpg', 'חסה.jpg', 'מלפפון.jpg', 'מנגו.jpg', 'עגבניה.jpg', 'פלפל אדום מתוק.jpg', 'פלפל ירוק חריף.jpg', 'צנון.jpg', 'שום.jpg'];
-const width = window.screen.width < 640 ? 90 : 100;
+const width = window.screen.width < 640 ? 97 : 100;
 const height = 100;
 const hiddenSrc = 'defulet card.png';
 let firstCard, secondCard;
